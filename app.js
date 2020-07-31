b57a2e0d05650e82a3d7d560a2c788cb2b1b9252
Gaurav Tewari <gauravtewari24@gmail.com>
2020-07-31 14:36:11 +0530

    initial

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+//const _ = require("lodash");
+const request = require('request');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", function (req, res) {
+    request('https://codeforces.com/api/problemset.problems?tags=dp', function (error, response, body) {
+        if (error) {
+            console.log("SOmething Wnt Wrong!!");
+            console.log(error);
+        }
+        else {
+            if (response.statusCode == 200) {
+                //console.log(body);
+                var data = JSON.parse(body);
+                res.render('dashboard', { data: data["result"]["problems"] });
+                //console.log(parsedData["result"]["problems"][0]);
+            }
+        }
+    })
+    //res.send("login");
+});
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
